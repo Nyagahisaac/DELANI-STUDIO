8c6a14aa652111a82a07530473467f6603494a52
isaac.ireri <isaac.ireri@student.moringaschool.com>
2021-07-24 06:05:56 +0300

    add Jquery to what we do img

---
+++ script.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $("#image").click(function () {
+        
+        $(".design-image").slideDown('1500').hide('1000');
+        $("#design").show('1500');
+       
+    });
+   
+    
+});
